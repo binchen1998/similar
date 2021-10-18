dc6e09ce6da0766232042ccba03f12091e471db6
binchen1998 <binchen1998>
2021-10-18 16:46:17 +0800

    first commit

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 let img;
 let v1 = [];
 let v2 = [];
+let model;
+let model_ready = false;
+let model_res;
+let img4;
 function preload() {
   img = loadImage('square.jpg');
 }
@@ -9,9 +13,54 @@ function setup() {
   // put setup code here
   canvas = createCanvas(400, 400);
 
-  image(img, 0, 0);
-  let c = get();
-  console.log(c);
+  //image(img, 0, 0);
+  //let c = get();
+  //console.log(c);
+
+  const MODEL_URL = 'model.json';
+
+  model = tf.loadGraphModel(MODEL_URL);
+  //const inCtx = canvas.getContext('2d');
+  // var element = document.getElementById("img1");
+  // let img1 = tf.browser.fromPixels(element, 3);
+  // img4 = tf.image.resizeBilinear(img1, [224, 224]).toFloat().div(tf.scalar(255));
+  // print(img4)
+  // let canvas2 = document.getElementById("canvas2");
+  // tf.browser.toPixels(img4,canvas2)
+  //img4 = img4.expand_dims(1);
+  //img4 = tf.reshape(img4, [1,224,224,3]);
+
+  model.then(function (res) {   
+    model_ready = true;
+    model_res = res;  
+  }, function (err) {
+      console.log(err);
+  });
+}
+
+function cmp()
+{
+  var element = document.getElementById("img1");
+  let img_pixels = tf.browser.fromPixels(element, 3);
+  let img1 = tf.image.resizeBilinear(img_pixels, [224, 224]).toFloat().div(tf.scalar(255));
+  img1 = tf.reshape(img1, [1,224,224,3]);
+
+  element = document.getElementById("img2");
+  img_pixels = tf.browser.fromPixels(element, 3);
+  let img2 = tf.image.resizeBilinear(img_pixels, [224, 224]).toFloat().div(tf.scalar(255));
+  img2 = tf.reshape(img2, [1,224,224,3]);
+
+  if (model_ready) {
+    const features1 =  Array.from(model_res.execute(img1).squeeze().dataSync());
+    console.log(features1);
+
+    const features2 = Array.from(model_res.execute(img2).squeeze().dataSync());
+    console.log(features2);
+
+    let si = cosine_similar(features1, features2);
+    console.log("result: ", si);
+  }
+
 }
 
 function print_data1()
@@ -63,6 +112,7 @@ function draw() {
 }
 
 function cosine_similar(vectorA = [], vectorB = []) {
+  console.log("vectorA: ", vectorA);
   const dimensionality = Math.min(vectorA.length, vectorB.length);
   let dotAB = 0;
   let dotA = 0;
